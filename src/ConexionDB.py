75c40d368f3993a3c4b46843095de6f95ba4c94e
JoseUrzua1708 <josejinloverdevera@gmail.com>
2025-08-07 03:54:47 -0600

    nabvar update 1.1

--- src/ConexionDB.py
+++ src/ConexionDB.py
@@ -8,7 +8,7 @@ def get_connection():
             host="localhost",
             port=3306,
             user="root",
-            password="12345",
+            password="Jose1708$",
             database="administracion"
         )
 
